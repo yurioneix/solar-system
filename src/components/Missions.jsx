978c2d6c77876e9f5f460369cab3b7ee882c5961
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-25 18:39:12 -0300

    Requisito 10: resolvido

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
@@ -7,6 +9,14 @@ class Missions extends Component {
       <>
         <div data-testid="missions" />
         <Title headline="Missões" />
+        {missions
+          .map((mission) => (<MissionCard
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+            key={ mission.name }
+          />))}
       </>
     );
   }
