55b56b13f0668eec13c2dcacbae39b9a7184f534
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-25 15:38:10 -0300

    Requisito 1: resolvido

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import Proptypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    // const { title } = this.props;
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+// Header.proptypes = {
+//   title: Proptypes.string.isRequired,
+// };
+
+export default Header;
